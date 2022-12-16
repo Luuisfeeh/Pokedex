232c1101998901adb6176e93daaa720a157df14c
Luis Felipe <103541121+Luuisfeeh@users.noreply.github.com>
2022-12-15 21:10:59 -0300

    Add files via upload

---
+++ Pokedex/js/script.js
@@ -0,0 +1,77 @@
+const pokemonName = document.querySelector('.pokemonName');
+const pokemonNumber = document.querySelector('.pokemonNumber');
+const pokemonImage = document.querySelector('.pokemonImage');
+
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.inputSearch');
+
+const buttonPrev = document.querySelector('.buttonPrev');
+const buttonNext = document.querySelector('.buttonNext');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async(pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`) ;
+
+    if (APIResponse.status == 200){
+        const data = await APIResponse.json();
+        return data;
+    } 
+    
+
+}
+
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data){
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        searchPokemon = data.id;
+
+        input.value = '';
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML ='Not found';
+        pokemonNumber.innerHTML = "000";
+    }
+     
+    
+
+}
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    renderPokemon(input.value.toLowerCase());
+
+
+});
+
+buttonPrev.addEventListener('click', () => {
+    if(searchPokemon > 1){
+        searchPokemon --;
+        renderPokemon(searchPokemon);
+    }
+    
+   
+});
+
+buttonNext.addEventListener('click', () => {
+   searchPokemon ++;
+   renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
+
+
+
